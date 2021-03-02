2996294df3933c9732d40438b7b0bcd0dac46ec0
anna9grace <anna9grace@gmail.com>
2021-03-02 19:57:22 +0300

    исправялет замечания по валидации формы

--- build/js/main.js
+++ build/js/main.js
@@ -195,29 +195,54 @@
 'use strict';
 
 (function () {
+  const forms = document.querySelectorAll(`.form`);
   const phoneFields = document.querySelectorAll(`#modal-phone, #customer-phone`);
   const PHONE_TEMPLATE_START = `+7(`;
   const PHONE_TEMPLATE_MIDDLE = `)`;
+  let isPhoneBeingWritten = false;
 
-  if (phoneFields.length < 1) {
-    return;
+  if (phoneFields.length > 0) {
+    for (const field of phoneFields) {
+      field.addEventListener(`focus`, (evt) => {
+        if (!evt.target.value) {
+          evt.target.value = PHONE_TEMPLATE_START;
+        }
+      });
+
+      field.addEventListener(`blur`, (evt) => {
+        if (evt.target.value === PHONE_TEMPLATE_START) {
+          evt.target.value = ``;
+        }
+      });
+
+      field.addEventListener(`input`, (evt) => {
+        if (evt.target.value.length === 5) {
+          isPhoneBeingWritten = true;
+        }
+        if (evt.target.value.length === 6 && isPhoneBeingWritten) {
+          evt.target.value += PHONE_TEMPLATE_MIDDLE;
+          isPhoneBeingWritten = false;
+        }
+      });
+    }
   }
 
-  for (const field of phoneFields) {
-    field.addEventListener(`focus`, (evt) => {
-      if (!evt.target.value) {
-        evt.target.value = PHONE_TEMPLATE_START;
-      }
-    });
-    field.addEventListener(`blur`, (evt) => {
-      if (evt.target.value === PHONE_TEMPLATE_START) {
-        evt.target.value = ``;
-      }
-    });
-    field.addEventListener(`input`, (evt) => {
-      if (evt.target.value.length === 6) {
-        evt.target.value += PHONE_TEMPLATE_MIDDLE;
-      }
-    });
+
+  if (forms.length > 0) {
+    for (const form of forms) {
+      const agreement = form.querySelector(`#modal-data-agreement`) || form.querySelector(`#data-agreement`);
+
+      agreement.addEventListener(`change`, () => {
+        agreement.setCustomValidity(
+            agreement.checked
+              ? ``
+              : `Для отправки согласитесь на обработку данных`
+        );
+      });
+
+      form.addEventListener(`submit`, () => {
+        agreement.reportValidity();
+      });
+    }
   }
 })();
