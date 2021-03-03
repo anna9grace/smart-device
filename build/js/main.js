a48ed4e8fa5b9234574745f733a6f3f9f46ead6f
anna9grace <anna9grace@gmail.com>
2021-03-03 09:39:52 +0300

    добавляет плавный скролл по ссылкам-якорям

--- build/js/main.js
+++ build/js/main.js
@@ -194,6 +194,25 @@
 
 'use strict';
 
+const anchors = document.querySelectorAll(`a[href*='#']`)
+
+if (anchors.length > 0) {
+	for (const anchor of anchors)	{
+		anchor.addEventListener( `click`, (evt) => {
+			evt.preventDefault();
+
+			const destinationId = anchor.getAttribute(`href`).substr(1);
+
+			document.getElementById(destinationId).scrollIntoView({
+				behavior: `smooth`,
+				block: `start`,
+			})
+		})
+	}
+}
+
+'use strict';
+
 (function () {
   const forms = document.querySelectorAll(`.form`);
   const phoneFields = document.querySelectorAll(`#modal-phone, #customer-phone`);
